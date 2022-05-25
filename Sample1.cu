75183e7d43e0acabfc87c204d76ee342090a6338
Aditi Das <93090825+aditi15901@users.noreply.github.com>
2022-05-25 21:10:32 +0530

    Add files via upload

---
+++ Sample1.cu
@@ -0,0 +1,33 @@
+int z;
+int gug(int z, char *v); // function declaration
+int main(int c, int b)
+{
+    char *b; /*yoloo */
+    int x = z[3] + 5; //huehueheu
+    b = a[9];
+    int a[10];
+    fun(x); // function call
+    if (x == 0)
+    {
+        char *c;
+    }
+    else
+    {
+        x = 0;
+    }
+    if (p == 'b')
+        c = p + f;
+
+    while (y != 0)
+    {
+        int p,q;
+    }
+
+    while (true)
+    y=-1;
+
+    while (0)
+    C='N';
+    
+    return 1000;
+}
---
+++ Sample2.cu
@@ -0,0 +1,25 @@
+int z;
+int main(int c, int b)
+{
+    char *b; /*yoloo */
+    int x = z[3] + 5; //huehueheu
+    b = a[9];
+    int a[10];
+    fun(x);
+    if (x == 0)%
+    {
+        char *c;
+    }
+    else
+    {
+        x = 0;
+    }
+    if (p == 'b')
+        c = p + f;
+
+    while (y != 0)
+    {
+        int p;
+    }
+    return 1000;
+}
